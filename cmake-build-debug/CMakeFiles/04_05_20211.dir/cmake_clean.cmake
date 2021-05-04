99ea871b3534a3a1f6f79341b080dd82d0acb101
Georgy Ruzavin <ruzavin02@mail.ru>
2021-05-04 15:18:14 +0500

    binary search

---
+++ cmake-build-debug/CMakeFiles/04_05_20211.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "04_05_20211.exe"
+  "04_05_20211.exe.manifest"
+  "04_05_20211.pdb"
+  "CMakeFiles/04_05_20211.dir/main.cpp.obj"
+  "lib04_05_20211.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/04_05_20211.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
